ef93f394efa59f149dcfd028d85ed85080a07d21
anantadwi13 <anantadwi13@yahoo.com>
2019-09-18 10:09:05 +0700

    done

--- c0/greet.py
+++ c0/greet.py
@@ -1,4 +1,6 @@
 import random
+import os
+import glob
 
 
 class GreetServer(object):
@@ -8,6 +10,55 @@ class GreetServer(object):
     def get_greet(self, name='NoName'):
         lucky_number = random.randint(1, 100000)
         return "Hello {}, this is your lucky number {}".format(name, lucky_number)
+
+    def create(self, filename, text):
+        if filename is None or filename == "":
+            return "Filename is not defined"
+        try:
+            with open(filename, 'x') as file:
+                file.write(text)
+                return "{} created".format(filename)
+        except Exception as e:
+            return e
+
+    def read(self, filename):
+        if filename is None or filename == "":
+            return "Filename is not defined"
+        try:
+            with open(filename, 'r') as file:
+                return file.read()
+        except Exception as e:
+            return e
+
+    def update(self, filename, text):
+        if filename is None or filename == "":
+            return "Filename is not defined"
+        try:
+            with open(filename, 'x') as file:
+                file.write(text)
+                return "{} updated".format(filename)
+        except Exception as e:
+            return e
+
+    def delete(self, filename):
+        if filename is None or filename == "":
+            return "Filename is not defined"
+        try:
+            os.remove(filename)
+            return "{} deleted".format(filename)
+        except Exception as e:
+            return e
+
+    def list(self, directory):
+        try:
+            files = [f for f in glob.glob(directory + "**/*", recursive=True)]
+            list_files = "\nFile Lists:"
+            for file in files:
+                list_files += "\n"+file
+
+            return list_files
+        except Exception as e:
+            return e
 
 
 if __name__ == '__main__':
--- c0/greet_client.py
+++ c0/greet_client.py
@@ -1,14 +1,42 @@
 import Pyro4
 
+
 def test_no_ns():
-    uri = "PYRO:obj_27d7c59497c44c688319f7d8a4a95935@localhost:40549"
+    uri = "PYRO:obj_c3a81e227eae4e05a9e0295633fefb23@localhost:64049"
     gserver = Pyro4.Proxy(uri)
     print(gserver.get_greet('ronaldo'))
+
 
 def test_with_ns():
     uri = "PYRONAME:greetserver@localhost:7777"
     gserver = Pyro4.Proxy(uri)
-    print(gserver.get_greet('ronaldo'))
+
+    run = True
+    while(run):
+        type = input("\nCommands:\n\t1 to create file\n\t2 to read file\n\t3 to update file\n\t4 to delete file\n\t5 to show list files\n\t6 to exit\n> ")
+        if type == "1":
+            filename = input("Filename : ")
+            text = input("Content : ")
+            print(gserver.create(filename, text))
+        elif type == "2":
+            filename = input("Filename : ")
+            print(gserver.read(filename))
+        elif type == "3":
+            filename = input("Filename : ")
+            text = input("Content : ")
+            print(gserver.update(filename, text))
+        elif type == "4":
+            filename = input("Filename : ")
+            print(gserver.delete(filename))
+        elif type == "5":
+            directory = input("Directory : ")
+            print(gserver.list(directory))
+        elif type == "6":
+            run = False
+            print("Exiting!")
+        else:
+            print("Command not found!")
+
 
 if __name__=='__main__':
     test_with_ns()
