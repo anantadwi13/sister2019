7dd2ac05bfea4bbd2cc74d14cbd6356d8cfc80f2
anantadwi13 <anantadwi13@yahoo.com>
2019-10-15 18:32:08 +0700

    centralized heartbeat

--- c0_centralized_heartbeat/client.py
+++ c0_centralized_heartbeat/client.py
@@ -3,77 +3,123 @@ from threading import Thread
 import time
 
 
-class HeartbeatDaemon:
+UNAVAILABLE_MSG = "Server is not available! Reconnecting..."
+
+
+class HeartbeatModel(object):
+    def __init__(self, server_id, seq, last_update):
+        self.server_id = server_id
+        self.seq = seq
+        self.last_update = last_update
+        self.is_alive = False
+
+    def update(self, seq, last_update):
+        self.seq = seq
+        self.last_update = last_update
+
+    def update_status(self, T):
+        if time.time() - self.last_update > 3 * T:
+            self.is_alive = False   # Unavailable
+        else:
+            self.is_alive = True    # Available
+
+
+class Heartbeat:
+    servers = dict()
+
+    def beat(self, server_id, seq_num):     # server_id must be a string
+        if server_id in Heartbeat.servers and isinstance(Heartbeat.servers[server_id], HeartbeatModel):
+            Heartbeat.servers[server_id].update(seq_num, int(time.time()))
+        else:
+            Heartbeat.servers[server_id] = HeartbeatModel(server_id, seq_num, int(time.time()))
+
+
+class HeartbeatDaemon(Thread):
     def __init__(self):
-        return
-
-
-class PingService(Thread):
-    def __init__(self, server):
         Thread.__init__(self)
-        self.server = server
-        self.server_is_alive = False
+
+    def run(self):
+        daemon = Pyro4.Daemon(host="localhost")
+        ns = Pyro4.locateNS("localhost", 7777)
+        x_ClientDaemon = Pyro4.expose(Heartbeat)
+        uri_fileserver = daemon.register(x_ClientDaemon)
+        print("URI file server : ", uri_fileserver)
+        ns.register("client", uri_fileserver)
+        daemon.requestLoop()
+
+
+class HeartbeatCheckerDaemon(Thread):
+    def __init__(self, T):      # T stands for time units
+        Thread.__init__(self)
+        self.T = T
 
     def run(self):
         while(True):
-            try:
-                self.server.ping()
-                self.server_is_alive = True
-                # print("ok")
-            except Exception as e:
-                self.server_is_alive = False
-                # print(e)
+            for server_id, server in Heartbeat.servers.items():
+                server.update_status(self.T)
+                # print(server.__dict__)
+            time.sleep(0.1)
 
 
 class ServerTask:
-    def __init__(self, server, ping_service):
+    def __init__(self, server, server_id):
         self.server = server
-        self.ping_service = ping_service
+        self.server_id = server_id
+
+    def is_available(self):
+        if not self.server_id in Heartbeat.servers or not Heartbeat.servers[self.server_id].is_alive:
+            return False
+        return True
 
     def create(self, filename, content):
-        if not self.ping_service.server_is_alive:
-            return "Server is not available! Reconnecting..."
+        if not self.is_available():
+            return UNAVAILABLE_MSG
         else:
             self.server.create(filename, content)
 
     def read(self, filename):
-        if not self.ping_service.server_is_alive:
-            return "Server is not available! Reconnecting..."
+        if not self.is_available():
+            return UNAVAILABLE_MSG
         else:
             self.server.read(filename)
 
     def update(self, filename, content):
-        if not self.ping_service.server_is_alive:
-            return "Server is not available! Reconnecting..."
+        if not self.is_available():
+            return UNAVAILABLE_MSG
         else:
             self.server.update(filename, content)
 
     def delete(self, filename):
-        if not self.ping_service.server_is_alive:
-            return "Server is not available! Reconnecting..."
+        if not self.is_available():
+            return UNAVAILABLE_MSG
         else:
             self.server.delete(filename)
 
     def list(self, directory):
-        if not self.ping_service.server_is_alive:
-            return "Server is not available! Reconnecting..."
+        if not self.is_available():
+            return UNAVAILABLE_MSG
         else:
             return self.server.list(directory)
 
 
 def test_with_ns():
-    uri = "PYRONAME:fileserver@localhost:7777"
+    t_units = 0.5
+    default_server_id = "server1"
+    uri = "PYRONAME:{0}@localhost:7777".format(default_server_id)
+
     fserver = Pyro4.Proxy(uri)
-    ping_service = PingService(fserver)
-    server_task = ServerTask(fserver, ping_service)
-    ping_service.start()
+    heartbeat_service = HeartbeatDaemon()
+    heartbeat_checker_service = HeartbeatCheckerDaemon(t_units)
+    server_task = ServerTask(fserver, default_server_id)
+    heartbeat_service.start()
+    heartbeat_checker_service.start()
 
     time.sleep(1)
 
     run = True
     while(run):
-        if not ping_service.server_is_alive:
-            print("Server is not available! Reconnecting...")
+        if not server_task.is_available():
+            print(UNAVAILABLE_MSG)
             time.sleep(1)
             continue
 
--- c0_centralized_heartbeat/server.py
+++ c0_centralized_heartbeat/server.py
@@ -1,27 +1,50 @@
 from c0_centralized_heartbeat.FileServer import  *
+from threading import Thread
+import time
 import Pyro4
 
 
-def start_without_ns():
-    daemon = Pyro4.Daemon()
-    x_FileServer = Pyro4.expose(FileServer)
-    uri = daemon.register(x_FileServer)
-    print("my URI : ", uri)
-    daemon.requestLoop()
+class HeartbeatDaemon(Thread):
+    def __init__(self, server_id, T):      # T stands for time units
+        Thread.__init__(self)
+        self.T = T
+        self.server_id = server_id
+
+    def run(self):
+        uri = "PYRONAME:client@localhost:7777"
+
+        client = Pyro4.Proxy(uri)
+
+        seq_no = 0
+        while(True):
+            try:
+                client.beat(self.server_id, seq_no)
+                seq_no += 1
+            except:
+                pass
+            time.sleep(self.T)
 
 
 def start_with_ns():
     #name server harus di start dulu dengan  pyro4-ns -n localhost -p 7777
     #gunakan URI untuk referensi name server yang akan digunakan
     #untuk mengecek service apa yang ada di ns, gunakan pyro4-nsc -n localhost -p 7777 list
+    t_units = 0.5
+
+
     daemon = Pyro4.Daemon(host="localhost")
-    ns = Pyro4.locateNS("localhost",7777)
     x_FileServer = Pyro4.expose(FileServer)
     uri_fileserver = daemon.register(x_FileServer)
     print("URI file server : ", uri_fileserver)
-    ns.register("fileserver", uri_fileserver)
-    daemon.requestLoop()
+    ns = Pyro4.locateNS("localhost", 7777)
 
+    for i in range(1, 6):
+        server_id = "server{0}".format(i)
+        ns.register(server_id, uri_fileserver)
+        heartbeat_service = HeartbeatDaemon(server_id, t_units)
+        heartbeat_service.start()
+
+    daemon.requestLoop()
 
 if __name__ == '__main__':
     start_with_ns()
