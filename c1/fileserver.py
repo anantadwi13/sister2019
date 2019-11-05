f76d7c8410a819e54e7fe1e2053fc72702db178d
Ananta Dwi Prasetya Purna Yuda <anantadwi13@yahoo.com>
2019-11-05 21:43:18 +0700

    sudah

--- c1/fileserver.py
+++ c1/fileserver.py
@@ -1,79 +1,120 @@
 import os
 import base64
+import Pyro4
+
 
 class FileServer(object):
+    CMD_CREATE = 1
+    CMD_UPDATE = 2
+    CMD_DELETE = 3
+
+    NAME = ""
+    DIR = ""
+    SERVER = []
+
     def __init__(self):
+        try:
+            os.mkdir(FileServer.DIR)
+        except:
+            pass
         pass
 
-    def create_return_message(self,kode='000',message='kosong',data=None):
-        return dict(kode=kode,message=message,data=data)
+    def get_fileserver_object(self, nama_server):
+        uri = "PYRONAME:{}@localhost:7777".format(nama_server)
+        fserver = Pyro4.Proxy(uri)
+        return fserver
+
+    def synchronize(self, cmd=0, data=None):
+        if data is None:
+            return
+        for server in FileServer.SERVER:
+            if server == FileServer.NAME:
+                continue
+            uri = "PYRONAME:{}@localhost:7777".format(server)
+            fserver = Pyro4.Proxy(uri)
+            if cmd == FileServer.CMD_CREATE:
+                fserver.create(**data)
+            elif cmd == FileServer.CMD_DELETE:
+                fserver.delete(**data)
+            elif cmd == FileServer.CMD_UPDATE:
+                fserver.update(**data)
+
+    def create_return_message(self, kode='000', message='kosong', data=None):
+        return dict(kode=kode, message=message, data=data)
 
     def list(self):
+        print(FileServer.DIR)
         print("list ops")
         try:
             daftarfile = []
-            for x in os.listdir():
-                if x[0:4]=='FFF-':
+            for x in os.listdir(FileServer.DIR):
+                if 'FFF-' in x:
                     daftarfile.append(x[4:])
-            return self.create_return_message('200',daftarfile)
+            return self.create_return_message('200', daftarfile)
         except:
-            return self.create_return_message('500','Error')
+            return self.create_return_message('500', 'Error')
 
-    def create(self, name='filename000'):
-        nama='FFF-{}' . format(name)
-        print("create ops {}" . format(nama))
+    def create(self, name='filename000', sync=True):
+        nama = 'FFF-{}'.format(name)
+        print("create ops {}".format(nama))
         try:
-            if os.path.exists(name):
-                return self.create_return_message('102', 'OK','File Exists')
-            f = open(nama,'wb',buffering=0)
+            if os.path.exists("{}{}".format(FileServer.DIR, nama)):
+                return self.create_return_message('102', 'OK', 'File Exists')
+            f = open("{}{}".format(FileServer.DIR, nama), 'wb', buffering=0)
             f.close()
-            return self.create_return_message('100','OK')
+            if sync:
+                self.synchronize(FileServer.CMD_CREATE, {"name": name, "sync": False})
+            return self.create_return_message('100', 'OK')
         except:
-            return self.create_return_message('500','Error')
-    def read(self,name='filename000'):
-        nama='FFF-{}' . format(name)
-        print("read ops {}" . format(nama))
+            return self.create_return_message('500', 'Error')
+
+    def read(self, name='filename000'):
+        nama = 'FFF-{}'.format(name)
+        print("read ops {}".format(nama))
         try:
-            f = open(nama,'r+b')
+            f = open("{}{}".format(FileServer.DIR, nama), 'r+b')
             contents = f.read().decode()
             f.close()
-            return self.create_return_message('101','OK',contents)
+            return self.create_return_message('101', 'OK', contents)
         except:
-            return self.create_return_message('500','Error')
-    def update(self,name='filename000',content=''):
-        nama='FFF-{}' . format(name)
-        print("update ops {}" . format(nama))
+            return self.create_return_message('500', 'Error')
 
-        if (str(type(content))=="<class 'dict'>"):
+    def update(self, name='filename000', content='', sync=True):
+        nama = 'FFF-{}'.format(name)
+        print("update ops {}".format(nama))
+
+        if (str(type(content)) == "<class 'dict'>"):
             content = content['data']
         try:
-            f = open(nama,'w+b')
+            f = open("{}{}".format(FileServer.DIR, nama), 'w+b')
             f.write(content.encode())
             f.close()
-            return self.create_return_message('101','OK')
+            if sync:
+                self.synchronize(FileServer.CMD_UPDATE, {"name": name, "content": content, "sync": False})
+            return self.create_return_message('101', 'OK')
         except Exception as e:
-            return self.create_return_message('500','Error',str(e))
+            return self.create_return_message('500', 'Error', str(e))
 
-    def delete(self,name='filename000'):
-        nama='FFF-{}' . format(name)
-        print("delete ops {}" . format(nama))
+    def delete(self, name='filename000', sync=True):
+        nama = 'FFF-{}'.format(name)
+        print("delete ops {}".format(nama))
 
         try:
-            os.remove(nama)
-            return self.create_return_message('101','OK')
+            os.remove("{}{}".format(FileServer.DIR, nama))
+            if sync:
+                self.synchronize(FileServer.CMD_DELETE, {"name": name, "sync": False})
+            return self.create_return_message('101', 'OK')
         except:
-            return self.create_return_message('500','Error')
-
+            return self.create_return_message('500', 'Error')
 
 
 if __name__ == '__main__':
     k = FileServer()
     print(k.create('f1'))
-    print(k.update('f1',content='wedusku'))
+    print(k.update('f1', content='wedusku'))
     print(k.read('f1'))
-#    print(k.create('f2'))
-#    print(k.update('f2',content='wedusmu'))
-#    print(k.read('f2'))
+    #    print(k.create('f2'))
+    #    print(k.update('f2',content='wedusmu'))
+    #    print(k.read('f2'))
     print(k.list())
-    #print(k.delete('f1'))
-
+    # print(k.delete('f1'))
